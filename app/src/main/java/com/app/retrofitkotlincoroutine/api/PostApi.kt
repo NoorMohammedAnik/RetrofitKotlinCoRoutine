89853d5c43b414d50c819b2b9c12550d01729efd
NOOR MOHAMMED ANIK <noormohammedanik@gmail.com>
2022-04-17 18:00:46 +0600

    Initial commit

---
+++ app/src/main/java/com/app/retrofitkotlincoroutine/api/PostApi.kt
@@ -0,0 +1,28 @@
+package com.app.retrofitkotlincoroutine.api
+
+
+import com.app.retrofitkotlincoroutine.Constants.END_POINT_DELETE
+import com.app.retrofitkotlincoroutine.Constants.END_POINT_GET
+import com.app.retrofitkotlincoroutine.Constants.END_POINT_POST
+import com.app.retrofitkotlincoroutine.model.PostItem
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface PostApi {
+
+    @GET(END_POINT_GET)
+    suspend fun getAllPosts(): Response<List<PostItem>>
+
+    @POST(END_POINT_POST)
+    suspend fun posting(
+        @Body postItem: PostItem
+    ): Response<PostItem>
+
+    @DELETE(END_POINT_DELETE)
+    suspend fun deletePost(): Response<ResponseBody>
+
+}
